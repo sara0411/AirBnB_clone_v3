c455f622f1334efea6dcb61c44f522964a3cc562
sara0411 <saraferraa0@gmail.com>
2024-01-09 22:54:56 +0100

    test documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,18 +7,22 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Returns JSON """
+    """Returns JSON indicating the status of the API."""
     return jsonify(status="OK")
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stat():
-    """returns the number of each objects by type"""
-    return jsonify(
-        amenities=storage.count('Amenity'),
-        cities=storage.count('City'),
-        places=storage.count('Place'),
-        reviews=storage.count('Review'),
-        states=storage.count('State'),
-        users=storage.count('User')
-    )
+    """Returns JSON with the number of objects for each type."""
+    try:
+        statistics = {
+            'amenities': storage.count('Amenity'),
+            'cities': storage.count('City'),
+            'places': storage.count('Place'),
+            'reviews': storage.count('Review'),
+            'states': storage.count('State'),
+            'users': storage.count('User')
+        }
+        return jsonify(statistics)
+    except Exception as e:
+        return jsonify(error=str(e)), 500  # Return a 500 Internal Server Error on exception
